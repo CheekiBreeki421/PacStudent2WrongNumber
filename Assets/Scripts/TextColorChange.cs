7653c78f4da648049cb3dc19039f7b8322b105ca
Lob <tntomodon12@gmail.com>
2020-10-22 14:51:13 +1100

    Finished title color changing, wroking on title movement

--- Assets/Scripts/TextColorChange.cs
+++ Assets/Scripts/TextColorChange.cs
@@ -5,22 +5,25 @@ using UnityEngine.UI;
 
 public class TextColorChange : MonoBehaviour
 {
-    public int ID;
+    public float ID;
     private Text text;
-    public Color color1;
-    public Color color2;
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<Text>();
-        color1 = new Color(0, 58, 115);
-        color2 = new Color(0, 154, 204);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-       text.color = Color.Lerp(new Color(0f, 0.227f, 0.45f), new Color(0f, 0.6f, 0.8f), Mathf.PingPong(Time.time, 1));
-        Debug.Log(text.color);
+        
+        if (Time.time >= ID) 
+        {
+            float timeFraction = Time.deltaTime / 5f;
+            text.color = Color.Lerp(new Color(0f, 0.227f, 0.45f), new Color(0f, 0.6f, 0.8f), Mathf.PingPong(Time.time/ID, 1));
+           
+        }
+      
     }
 }
---
+++ Assets/WanderingText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingText : MonoBehaviour
+{
+    private RectTransform ract;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ract = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ract.position = Vector2.Lerp(new Vector2(ract.position.x, ract.position.y), new Vector2(ract.position.x-Random.Range(0,0.2f), ract.position.y - Random.Range(0, 0.2f)), Time.deltaTime/5f); 
+    }
+}
